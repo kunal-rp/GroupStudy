a980ecb653e6de6154b77c1c1b920311437b4c86
Kunal <kunal@b98-aruba-authenticated-10-110-159-155.wlan.cpp.edu>
2017-12-02 12:25:04 -0800

    Response when add USer

--- server/server.js
+++ server/server.js
@@ -4,8 +4,11 @@ var path = require('path');
 var bcrypt = require('bcrypt');
 
 
+
+
+
 process.on('uncaughtException', function (err) {
-  console.log(err);
+	console.log(err);
 });
 
 app = express();
@@ -27,56 +30,68 @@ var connectionPool = mysql.createPool({
 var table_user = "Users"
 
 
-function generateUserID(data,connection){
-    var number =  Math.floor(Math.random() * 16777216)+1;
-    number = number.toString(16).toUpperCase();
 
-    var query_user_prim = "Select * From "+ table_user + " Where `user_id` ='"+number+"'";
-    connection.query(query_user_prim,function(err, results){
-        if(err){
-            console.log("ERROR | Generating User ID |"+err)
-            generateUserID(data,connection)
-        }
-        else{
-            if(results.length == 0){
 
-				data['user_id'] = number;
-				addUser(data,connection)
+
+app.get('/addUser',function(req,res){
+	var data = {user_name:"Kunal Purohit",user_password:"password"}
+	connectionPool.getConnection(function(connection_error,connection){
+		if(connection_error){
+			console.log(connection_error)
+		}
+		else{
+			generateUserID(data,connection,function(){res.send("Done")})
+		}
+	})
+
+});
+
+
+console.log("Server Running ")
+
+
+//_________\\
+
+function generateUserID(data,connection,callback)
+    {
+        var number =  Math.floor(Math.random() * 16777216)+1;
+        number = number.toString(16).toUpperCase();
+
+        var query_user_prim = "Select * From "+ table_user + " Where `user_id` ='"+number+"'";
+        connection.query(query_user_prim,function(err, results){
+            if(err){
+                console.log("ERROR | Generating User ID |"+err)
+                addUser(data,connection,callback)
             }
             else{
-                generateUserID(data,connection)
+                if(results.length == 0){
+
+                    data['user_id'] = number;
+                    callback(data,connection)
+                }
+                else{
+                    generateUserID(data,connection,callback)
+                }
             }
-        }
-    });
-}
+        });
+    }
 
-
-function addUser(data,connection){
+function addUser(data,connection,callback){
     var user_id = data.user_id;
     var user_name = data.user_name;
     var user_password = data.user_password;
     var query_add_prim = "INSERT INTO "+table_user + "(`user_id`, `user_name`, `user_password`, `subscribed_groups`) VALUES('"+user_id+"','"+user_name+"','"+user_password+"','-')";
     connection.query( query_add_prim , function(err_prim,results_prim) {
-        if(err_prim){    
-            console.log("ERROR | inserting new user |"+err_prim + "|"+query_add_prim)
-            callback(err_prim)
-        }
-        else{
-            console.log("user added :"+data.toString())
-        }  
+      if(err_prim){    
+        console.log("ERROR | inserting new user |"+err_prim + "|"+query_add_prim)
+        callback(err_prim)
+      }
+      else{
+        console.log("user added :"+data.toString())
+        callback()
+      }  
     });
-}
+  }
 
-app.get('/test',function(req,res){
-   var data = {user_name:"Kunal Purohit",user_password:"password"}
-   connectionPool.getConnection(function(connection_error,connection){
-   		if(connection_error){
-   			console.log(connection_error)
-   		}
-   		else{
-   			  generateUserID(data,connection)
-   		}
-   })
 
-});
-console.log("Server Running ")
+
