4a03e77a7a486fb0e3a1fdc87016f17d4e7861ee
anbe6083 <anbe6083@colorado.edu>
2017-12-02 11:07:15 -0800

    Added main_window and side_bar

---
+++ group_study/src/main_window.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+export default class Main_Window extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className='main-window-container'>
+                Main Window
+            </div>
+        )
+    }
+}
---
+++ group_study/src/sidebar.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import './App.css';
+
+export default class SideBar extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className='sidebar-container'>
+                Sidebar
+                
+            </div>
+        )
+    }
+}
